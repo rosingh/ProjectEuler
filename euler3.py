93a97d21ff20a05c457866d49f29bf5e4cc4c203
Rodrigo Singh <rodrigo0singh@gmail.com>
2014-01-23 17:13:56 -0500

    Made solution to #3 a bit more readable.

--- euler3.py
+++ euler3.py
@@ -1,9 +1,9 @@
 def main():
-	factorize = 600851475143
+	factorize = 15
 	# Start at 2 to begin checking for prime factors
 	factor = 2
 	# If the divisor equals the dividend, it's prime
-	while factorize != factor:
+	while factor < factorize:
 		# If it is a factor, divide the number by the factor
 		if factorize % factor == 0:
 			factorize /= factor
